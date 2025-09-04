fb1ef89cc14e5d316855e31eb22b4d3c0d54837e
MarcoMorbach <marco.morbach@sempreceub.com>
2025-09-03 21:38:09 -0300

    Primeira versão

---
+++ Teste.java
@@ -0,0 +1,14 @@
+package testegit;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		int soma = a + b;
+		
+		System.out.println(soma);
+	}
+
+}
