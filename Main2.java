bf53adeb42a8d56e6e6918ecb2e153ccc85dab99
Marco Antonio Chaves Morbach <marco.morbach@dinfor.uniceub>
2025-09-12 10:46:01 -0300

    Primeiro exercicio

---
+++ Main2.java
@@ -0,0 +1,23 @@
+package aula6_exercicio1_arraylist;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> num = new ArrayList<Integer>();
+		num.add(11);
+		num.add(12);
+		num.add(20);
+		num.add(21);
+		
+		System.out.println(num.size());
+		
+		if(num.contains(11)) {
+			System.out.println("O numero 11 esta na lista");
+		}
+		//MELHORAR
+	}
+
+}
